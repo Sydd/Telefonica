eb3115b47071230b90f29a645e9c24445ad9b808
santi.rosales <santiago.rosales@boxitsoft.com>
2020-05-03 17:54:24 -0300

    Subiendo proyecto baseee

---
+++ src/main/java/utn/telefonica/app/TelefonicaApplication.java
@@ -0,0 +1,13 @@
+package utn.telefonica.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefonicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/telefonica/app/TelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.telefonica.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
